5e70ec79f6c1d1c232d839134f2c60028af26a8f
KhayT <tkprof4@gmail.com>
2022-06-27 18:44:51 -0700

    ..

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Dice } from "./component/svg";
+
+const start = {
+  id: 71,
+  advice:
+    " It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+};
 
 function App() {
+  const [advice, setAdvice] = useState(start);
+
+  const changeAdviceHandler = async () => {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+
+    setAdvice(data.slip);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 className="title">Advice {advice.id}</h3>
+      <p className="advice">"{advice.advice}"</p>
+      <div className="divider"></div>
+      <div className="dice-holder" onClick={changeAdviceHandler}>
+        <Dice className="dice-icon" />
+      </div>
     </div>
   );
 }
